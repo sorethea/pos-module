11dca3e9472cb37cf55131131c78e071218e12be
Sorethea OK <sorethea@gmail.com>
2023-01-27 15:10:09 +0700

    lam-dev

--- Database/Migrations/2023_01_27_080911_create_foods_table.php
+++ Database/Migrations/2023_01_27_080911_create_foods_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('food', function (Blueprint $table) {
+        Schema::create('foods', function (Blueprint $table) {
             $table->id();
 
             $table->timestamps();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('food');
+        Schema::dropIfExists('foods');
     }
 };
